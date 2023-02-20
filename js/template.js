3a7f8b859578fac8adf60ca7f83980dd28b81496
Sheikh-Md-Asif-Abrar <121845817+Sheikh-Md-Asif-Abrar@users.noreply.github.com>
2023-02-20 12:26:51 +0600

    Update

---
+++ js/template.js
@@ -0,0 +1,26 @@
+const person = 'Adam Sandler';
+const person2 = "Ben White";
+const person3 = "BDonald Trump";
+
+const multiLine = 'First Line Text \n' +
+'Second line of code \n' +
+'Third line of text \n' + 
+'Fourth line of string';
+// console.log(multiline)
+
+
+const newMultiline = `First Line Text  
+Second line of code
+Third line of text 
+Fourth line of string`;
+
+// console.log(newMultiline)
+
+const a = 20;
+const b = 30;
+const nums = [78, 98, 45, 12, 6, 444]
+const summary = 'sum of: ' + a + ' and ' + b + ' is: '+ (a+b);
+// console.log(summary)
+
+const newSummary = `sum of ${a} and ${nums.length} is: ${a+b}`
+console.log(newSummary)
